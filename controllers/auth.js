20d130e12c067c9153aff58d5182896e54254e75
April Yuen <april.you@gmail.com>
2022-09-19 21:56:35 -0400

    Added login and signup auth pages.

---
+++ controllers/auth.js
@@ -0,0 +1,105 @@
+const passport = require("passport");
+const validator = require("validator");
+const { rawListeners } = require("../models/Story");
+const User = require("../models/User");
+
+exports.getLogin = (req, res) => {
+    if(req.user) {
+        return res.redirect("/dashboard");
+    }
+    res.render("login", {
+        title: "Login"
+    })
+};
+
+exports.postLogin = (req, res, next) => {
+    const validationErrors = [];
+    if (!validator.isEmail(req.body.email))
+      validationErrors.push({ msg: "Please enter a valid email address." });
+    if (validator.isEmpty(req.body.password))
+      validationErrors.push({ msg: "Password cannot be blank." });
+  
+    if (validationErrors.length) {
+      req.flash("errors", validationErrors);
+      return res.redirect("/login");
+    }
+    req.body.email = validator.normalizeEmail(req.body.email, {
+      gmail_remove_dots: false,
+    });
+  
+    passport.authenticate("local", (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        req.flash("errors", info);
+        return res.redirect("/login");
+      }
+      req.logIn(user, (err) => {
+        if (err) {
+          return next(err);
+        }
+        req.flash("success", { msg: "Success! You are logged in." });
+        res.redirect(req.session.returnTo || "/profile");
+      });
+    })(req, res, next);
+  };
+
+exports.getSignup = (req, res) => {
+    if(req.user){
+        return res.redirect("/dashboard");
+    }
+    res.render("signup", {
+        title: "Create Account",
+    })
+};
+
+exports.postSignup = (req, res, next) => {
+    const validationErrors = [];
+    if(!validator.isEmail(req.body.email))
+        validationErrors.push({msg: "Please enter a valid email address."});
+    if(!validator.isLength(req.body.password, {min: 8}))
+        validationErrors.push({
+            msg: "Password must be at least 8 characters long", 
+        });
+    if(req.body.password !== req.body.confirmPassword)
+        validationErrors.push({msg: "Passwords do not match"})
+    
+    if(validationErrors.length) {
+        req.flash("errors", validationErrors);
+        return res.redirect("../signup");
+    }
+    req.body.email = validator.normalizeEmail(req.body.email, {
+        gmail_remove_dots: false,
+    });
+
+    User.findOne(
+        {$or: [{email: req.body.email}, { username: req.body.userName}] }, 
+        (err, existingUser) => {
+            if(err){
+                return next(err);
+            }
+            if(existingUser){
+                req.flash("errors", {
+                    msg: "Account with that email address or username already.", 
+                });
+                return res.redirect("../signup")
+            }
+            user.save((err) => {
+                if(err) {
+                    return next(err);
+                }
+                req.logIn(user, (err) => {
+                    if(err){
+                        return next(err);
+                    }
+                    res.redirect("/dashboard")
+                })
+            })
+        }
+    )
+}
+
+
+
+
